c6262e9db895ab76f9852f4ce9db0bb8de3149e6
Haxend <45284621+Haxend@users.noreply.github.com>
2022-07-27 22:44:36 +0300

    Добавьте файлы проекта.

---
+++ BinaryTree_Test/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BinaryTree_Test
+{
+    public class Programm
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree<int>();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(1);
+            tree.Add(2);
+            tree.Add(8);
+            tree.Add(6);
+            tree.Add(9);
+
+            ////////////////
+            //      5
+            //     / \
+            //   3     7
+            //  / \   / \
+            // 1   2 6   8
+            //            \
+            //             9
+            ////////////////
+            foreach (var item in tree.PreOrder())
+            { 
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in tree.PostOrder())
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in tree.InOrder())
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
---
+++ BinaryTree_Test/Tree.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree_Test
+{
+    class Tree<T>
+        where T : IComparable, IComparable<T>
+    {
+        public Node<T> Root { get; set; }
+        public int Count { get; set; }
+
+        public void Add(T data)
+        { 
+            var node = new Node<T>(data);
+
+            if (Root == null)
+            { 
+                Root = new Node<T>(data);
+                Count = 1;
+                return;
+            }
+
+            Root.Add(data);
+            Count++;
+        }
+
+        public List<T> PreOrder()
+        {
+            if (Root == null)
+            {
+                return new List<T>();
+            }
+            return PreOrder(Root);
+        }
+
+        public List<T> PreOrder(Node<T> node)
+        {
+            var list = new List<T>();
+
+            if (node != null)
+            {
+                list.Add(node.Data);
+
+                if (node.Left != null)
+                {
+                    list.AddRange(PreOrder(node.Left));
+                }
+
+                if (node.Right != null)
+                {
+                    list.AddRange(PreOrder(node.Right));
+                }
+            }
+            return list;
+        }
+
+        public List<T> PostOrder()
+        {
+            if (Root == null)
+            {
+                return new List<T>();
+            }
+            return PostOrder(Root);
+        }
+
+        public List<T> PostOrder(Node<T> node)
+        {
+            var list = new List<T>();
+
+            if (node != null)
+            {
+                if (node.Left != null)
+                {
+                    list.AddRange(PostOrder(node.Left));
+                }
+
+                if (node.Right != null)
+                {
+                    list.AddRange(PostOrder(node.Right));
+                }
+
+                list.Add(node.Data);
+            }
+            return list;
+        }
+
+        public List<T> InOrder()
+        {
+            if (Root == null)
+            {
+                return new List<T>();
+            }
+            return InOrder(Root);
+        }
+
+        public List<T> InOrder(Node<T> node)
+        {
+            var list = new List<T>();
+
+            if (node != null)
+            {
+                if (node.Left != null)
+                {
+                    list.AddRange(InOrder(node.Left));
+                }
+
+                list.Add(node.Data);
+
+                if (node.Right != null)
+                {
+                    list.AddRange(InOrder(node.Right));
+                }
+            }
+            return list;
+        }
+    }
+}
